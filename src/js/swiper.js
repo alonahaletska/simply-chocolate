3ae757853b5957e7b7dcf538a9d3cc15af21b36d
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-06-02 23:24:41 +0300

    mobile style section

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -19,4 +19,22 @@ const swiper = new Swiper('.swiper', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+
+  // spaceBetween: -400,
+
+variableWidth:true,
+  
+    breakpoints: {
+    320: {
+      sliderPerView: 1,
+    },
+    768: {
+      sliderPerView: 2.5,
+    },
+    1200: {
+      sliderPerView: 4,
+    }
+  },
+
+  
 });
